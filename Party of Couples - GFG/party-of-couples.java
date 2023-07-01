8474f23424c29ae0807a021fed45e32f2f380665
mohitagal98 <64399196+mohitagal98@users.noreply.github.com>
2023-07-01 15:42:29 +0530

    Added solution - LeetHub

---
+++ Party of Couples - GFG/party-of-couples.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.findSingle(N, arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int findSingle(int n, int nums[]){
+        Arrays.sort(nums);
+        if(nums.length == 1)
+            return nums[0];
+        for(int i = 0; i < nums.length - 1; i+=2)
+        {
+            if(nums[i] != nums[i+1])
+                return nums[i];
+        }
+        return nums[nums.length-1];
+    }
+}
