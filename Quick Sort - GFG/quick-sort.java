48915ca92f0131c5a9c817d71a401a867a913111
mohitagal98 <64399196+mohitagal98@users.noreply.github.com>
2023-07-06 22:30:38 +0530

    Added solution - LeetHub

---
+++ Quick Sort - GFG/quick-sort.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.util.*;
+class Sorting
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    
+    // Driver program
+    public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			
+			new Solution().quickSort(arr,0,n-1);
+			printArray(arr);
+		    T--;
+		}
+} }
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high)
+    {
+        if (low < high) {
+  
+            // pi is partitioning index, arr[p]
+            // is now at right place
+            int pi = partition(arr, low, high);
+  
+            // Separately sort elements before
+            // partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    static int partition(int arr[], int low, int high)
+    {
+        int pivot = arr[high];
+  
+        // Index of smaller element and indicates
+        // the right position of pivot found so far
+        int i = (low - 1);
+  
+        for (int j = low; j <= high - 1; j++) {
+  
+            // If current element is smaller than the pivot
+            if (arr[j] < pivot) {
+  
+                // Increment index of smaller element
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+                arr[i+1] = arr[high];
+                arr[high] = temp;
+        return (i + 1);
+    } 
+}
