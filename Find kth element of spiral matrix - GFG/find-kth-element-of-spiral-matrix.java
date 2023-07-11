86ff484ae0445fab94e9215357b38527c97932d0
mohitagal98 <64399196+mohitagal98@users.noreply.github.com>
2023-07-11 18:08:45 +0530

    Added solution - LeetHub

---
+++ Find kth element of spiral matrix - GFG/find-kth-element-of-spiral-matrix.java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Find_Given_Element_Of_Spiral_Matrix 
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t > 0)
+		{
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int k = sc.nextInt();
+			int arr[][] = new int[1000][1000];
+			for(int i=0; i<n; i++)
+			{
+				for(int j=0; j<m; j++ )
+				{
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			System.out.println(new Solution().findK(arr, n, m, k));
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    /*You are required to complete this method*/
+    int findK(int A[][], int n, int m, int k)
+    {
+        int l = 0, i = 0, j = 0, count = 0;
+        boolean hor = true, ver = true;
+    	while(n > 0 || m > 0)
+    	{
+    	    if(l % 2 == 0)// horizontally
+    	    {
+    	        if(hor)//horizontally forward
+    	        {
+    	            for( int a = 0 ; a < m; a++, j++)
+    	            {
+    	                count++;
+    	                if(count == k)
+    	                    return A[i][j];
+    	            }
+    	            j--;
+    	            i++;
+    	            n--;
+    	            hor = false;
+    	        }
+    	        else// horizontally backward
+    	        {
+    	            for(int a = 0; a < m; a++, j--)
+    	            {
+    	                count++;
+    	                if(count == k)
+    	                    return A[i][j];
+    	            }
+    	            j++;
+    	            i--;
+    	            n--;
+    	            hor = true;
+    	        }
+    	        l++;
+    	    }
+    	    else //vertically
+    	    {
+    	        if(ver)//vertically downward
+    	        {
+    	            for(int b = 0 ; b < n; b++, i++)
+    	            {
+    	                count++;
+    	                if(count == k)
+    	                    return A[i][j];
+    	                   
+    	            }
+    	            i--;
+    	            j--;
+    	            m--;
+    	            ver = false;
+    	        }
+    	        else // vertically upward
+    	        {
+    	            for(int b = 0; b < n; b++, i--)
+    	            {
+    	                count++;
+    	                if(count == k)
+    	                    return A[i][j];
+    	               
+    	            }
+    	            i++;
+    	            j++;
+    	            m--;
+    	            ver= true;
+    	        }
+    	        l++;
+    	    }
+    	}
+    	return 0;
+    }
+}
