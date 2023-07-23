646259bdf2f662c93c9e47165a1b7e413304cc15
mohitagal98 <64399196+mohitagal98@users.noreply.github.com>
2023-07-23 23:23:58 +0530

    Added solution - LeetHub

---
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..java
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n =Integer.parseInt(in.readLine().trim());
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(s[i]));
+                tail = tail.next;
+            }
+
+            head = new Solution().segregate(head);
+            printList(head, out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node head,PrintWriter out)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    
+    static Node segregate(Node head)
+    {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while(head != null)
+        {
+            pq.add(head.data);
+            head = head.next;
+        }
+        head = new Node(pq.remove());
+        Node temp = head;
+        Iterator itr = pq.iterator();
+        while(itr.hasNext())
+        {
+            temp.next = new Node(pq.remove());
+            temp = temp.next;
+        }
+        return head;
+    }
+}
+
+
