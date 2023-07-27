a8e9ee185943ed51c45db5571927ed0ba9f1dd93
mohitagal98 <64399196+mohitagal98@users.noreply.github.com>
2023-07-27 15:42:54 +0530

    Time: 1 ms (80.00%), Space: 45.2 MB (17.06%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -10,22 +10,29 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        if(head == null)
-            return head;
-        ListNode temp = head;
-        while(temp.next != null)
-        {
-            if(temp.next.val == val)
-            {
-                temp.next = temp.next.next;
-                continue;
-            }
-            temp = temp.next;
-        }
-        if(head.val == val)
-        {
-            head = head.next;
-        }
-        return head;
+        
+    // Recursive approach
+        if (head == null) return null;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+        
+        
+        // if(head == null)
+        //     return head;
+        // ListNode temp = head;
+        // while(temp.next != null)
+        // {
+        //     if(temp.next.val == val)
+        //     {
+        //         temp.next = temp.next.next;
+        //         continue;
+        //     }
+        //     temp = temp.next;
+        // }
+        // if(head.val == val)
+        // {
+        //     head = head.next;
+        // }
+        // return head;
     }
 }
