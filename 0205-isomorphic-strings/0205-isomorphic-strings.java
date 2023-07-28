8c3e400a10073a53afc791402d638612ae4220eb
mohitagal98 <64399196+mohitagal98@users.noreply.github.com>
2023-07-28 12:04:54 +0530

    Time: 7 ms (88.84%), Space: 41.8 MB (29.91%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,25 +1,35 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> hashMap = new HashMap<>();
-        for(int i = 0; i < s.length(); i++)
+//         Map<Character, Character> hashMap = new HashMap<>();
+//         for(int i = 0; i < s.length(); i++)
+//         {
+//             char key = s.charAt(i);
+//             char value = t.charAt(i);
+//             if(hashMap.containsKey(key) && hashMap.get(key) != value)
+//             {
+
+//                 return false;
+//             }
+//             else if(!hashMap.containsKey(key) && hashMap.containsValue(value))
+//             {
+
+//                 return false;
+//             }
+//             else
+//             {
+
+//                 hashMap.put(key, value);
+//             }
+//         }
+//         return true;
+        int[] a = new int[256];
+        int b[] = new int[256];
+        for(int i = 0; i < s.length(); i ++)
         {
-            char key = s.charAt(i);
-            char value = t.charAt(i);
-            if(hashMap.containsKey(key) && hashMap.get(key) != value)
-            {
-
+            if(a[s.charAt(i)] != b[t.charAt(i)])
                 return false;
-            }
-            else if(!hashMap.containsKey(key) && hashMap.containsValue(value))
-            {
-
-                return false;
-            }
-            else
-            {
-
-                hashMap.put(key, value);
-            }
+            a[s.charAt(i)] = i+1;
+            b[t.charAt(i)] = i+1;
         }
         return true;
     }
