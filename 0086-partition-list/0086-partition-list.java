b83a911f9d1ef74dbed47040547d0ce89ef76995
mohitagal98 <64399196+mohitagal98@users.noreply.github.com>
2023-07-28 16:12:08 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (10.06%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -10,41 +10,64 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode prev = null, large = head;
-        if(head == null)
-            return head;
-        while(large != null && large.val < x)
-        {
-            prev = large;
-            large = large.next;
+//         ListNode prev = null, large = head;
+//         if(head == null)
+//             return head;
+//         while(large != null && large.val < x)
+//         {
+//             prev = large;
+//             large = large.next;
+//         }
+        
+//         if(large == null || large.next == null)
+//             return head;
+//         while(large != null && large.next != null)
+//         {
+            
+//             if(large.next.val < x)
+//             {
+//                 ListNode temp = large.next;
+//                 large.next = large.next.next;
+//                 if(prev == null)
+//                 {
+//                     System.out.println(0);
+//                     temp.next = head;
+//                     head = temp;
+//                     prev = head;
+//                 }
+//                 else
+//                 {
+//                     temp.next = prev.next;
+//                     prev.next = temp;
+//                     prev = prev.next;
+//                 }
+//                 continue;
+//             }
+//             large = large.next;
+//         }
+//         return head;
+        ListNode left = new ListNode(0);
+        ListNode right = new ListNode(0);
+        
+        ListNode leftTail = left;
+        ListNode rightTail = right;
+        
+        while(head != null){
+            if(head.val < x){
+                leftTail.next = head;
+                leftTail = leftTail.next;
+            }
+            else{
+                rightTail.next = head;
+                rightTail = rightTail.next;
+            }
+            head = head.next;
         }
         
-        if(large == null || large.next == null)
-            return head;
-        while(large != null && large.next != null)
-        {
-            
-            if(large.next.val < x)
-            {
-                ListNode temp = large.next;
-                large.next = large.next.next;
-                if(prev == null)
-                {
-                    System.out.println(0);
-                    temp.next = head;
-                    head = temp;
-                    prev = head;
-                }
-                else
-                {
-                    temp.next = prev.next;
-                    prev.next = temp;
-                    prev = prev.next;
-                }
-                continue;
-            }
-            large = large.next;
-        }
-        return head;
+        leftTail.next = right.next;
+        rightTail.next = null;
+        
+        return left.next;
+    
     }
 }
