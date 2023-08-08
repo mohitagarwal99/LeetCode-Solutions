31bb0009b8063684dc23fbb93907412ce3df38c0
mohitagal98 <64399196+mohitagal98@users.noreply.github.com>
2023-08-08 18:42:48 +0530

    Added solution - LeetHub

---
+++ Fraction pairs with sum 1 - GFG/fraction-pairs-with-sum-1.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] numerator = IntArray.input(br, n);
+            
+            
+            int[] denominator = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.countFractions(n, numerator, denominator);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int countFractions(int n, int[] num, int[] den) {
+        HashMap<Double, Integer> hmap = new HashMap<>();
+        int count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            double key = (double)num[i] / (double) den[i];
+            double key2 = (double)(den[i] - num[i]) / den[i];
+
+            if(hmap.containsKey(key2))
+                count += hmap.get(key2);
+            hmap.put(key, hmap.getOrDefault(key, 0) +1);
+        }
+        
+        return count;
+    }
+}
+        
