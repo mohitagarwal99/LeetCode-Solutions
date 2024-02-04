ead72ed36dcbe69f5bb7b748dc3c7ed83b2c4efc
Mohit Agarwal <64399196+mohitagarwal99@users.noreply.github.com>
2024-02-04 13:47:08 +0530

    Time: 35 ms (51.01%), Space: 67.8 MB (7.75%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,29 +1,46 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0)
-            return 0;
-        TreeSet<Integer> list = new TreeSet<>();
-        for(int n: nums){
-            list.add(n);
-        }
-        Iterator<Integer> itr = list.iterator();
-        int count = 1; 
-        int max = 1;
-        int prev = itr.next();
-        while(itr.hasNext()){
-            int next = itr.next();
-            if(prev + 1 == next)
-            {
-                count++;
-            }
+//         if(nums.length == 0)
+//             return 0;
+//         TreeSet<Integer> list = new TreeSet<>();
+//         for(int n: nums){
+//             list.add(n);
+//         }
+//         Iterator<Integer> itr = list.iterator();
+//         int count = 1; 
+//         int max = 1;
+//         int prev = itr.next();
+//         while(itr.hasNext()){
+//             int next = itr.next();
+//             if(prev + 1 == next)
+//             {
+//                 count++;
+//             }
                 
-            else{
-                count = 1;
-            }
-            prev = next;
-            max = Math.max(max, count);
+//             else{
+//                 count = 1;
+//             }
+//             prev = next;
+//             max = Math.max(max, count);
             
+//         }
+        // return max;
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i : nums){
+            set.add(i);
         }
-        return max;
+        int max = 0, cnt = 0;
+        for(int it : set){
+            if(!set.contains(it-1)){
+                cnt = 1;
+                int x = it;
+                while(set.contains(x+1)){
+                    cnt++;
+                    x++;
+                }
+                max = Math.max(max, cnt);
+            }
+        }
+       return max;
     }
 }
