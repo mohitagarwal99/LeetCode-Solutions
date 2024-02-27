321c3086dce6849df1ca860547cb22b0c1de35ba
Mohit Agarwal <64399196+mohitagarwal99@users.noreply.github.com>
2024-02-27 10:41:43 +0530

    Time: 1 ms (99.93%), Space: 48.2 MB (74.99%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -12,45 +12,71 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         
-        ListNode temp = headA;
-        int lenA = 0, lenB = 0;
+//         ListNode temp = headA;
+//         int lenA = 0, lenB = 0;
         
-        while(temp != null){
-            temp = temp.next;
-            lenA++;
-        }
+//         while(temp != null){
+//             temp = temp.next;
+//             lenA++;
+//         }
         
-        temp = headB;
-        while(temp != null){
-            temp = temp.next;
-            lenB++;
-        }
+//         temp = headB;
+//         while(temp != null){
+//             temp = temp.next;
+//             lenB++;
+//         }
         
-        if(lenA > lenB)
-        {
-            temp = intersectingNode(headA, headB, lenA-lenB);
-        }
-        else
-        {
-            temp = intersectingNode(headB, headA, lenB-lenA);
-        }
+//         if(lenA > lenB)
+//         {
+//             temp = intersectingNode(headA, headB, lenA-lenB);
+//         }
+//         else
+//         {
+//             temp = intersectingNode(headB, headA, lenB-lenA);
+//         }
             
-        return temp;
-    }
-    public ListNode intersectingNode(ListNode headA, ListNode headB, int skip){
+//         return temp;
         
-        while(skip-- != 0){
-            headA = headA.next;
-        }
         
-        while(headA != null){
-            if(headA == headB)
-                return headA;
-            headA = headA.next;
-            headB = headB.next;
+        //Optimized version:
+        ListNode tempA = headA, tempB = headB;
+        
+        while(tempA != null || tempB != null){
+            
+            if(tempA == tempB)
+                return tempA;
+            
+            if(tempA == null)
+                tempA= headB;
+            else
+            {
+                tempA = tempA.next;
+            }
+            
+            if(tempB == null)
+                tempB = headA;
+            else
+            {
+                tempB = tempB.next;
+            }
         }
         
         return null;
-        
     }
+//     public ListNode intersectingNode(ListNode headA, ListNode headB, int skip){
+        
+//         while(skip-- != 0){
+//             headA = headA.next;
+//         }
+        
+//         while(headA != null){
+//             if(headA == headB)
+//                 return headA;
+//             headA = headA.next;
+//             headB = headB.next;
+//         }
+        
+//         return null;
+        
+//     }
 }
