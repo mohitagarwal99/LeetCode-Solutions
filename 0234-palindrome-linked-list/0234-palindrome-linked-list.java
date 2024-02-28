05b05a54a803226c991bff276641092fd335f3b3
Mohit Agarwal <64399196+mohitagarwal99@users.noreply.github.com>
2024-02-28 12:44:59 +0530

    Time: 5 ms (64.17%), Space: 68.4 MB (49.19%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,24 +9,56 @@
  * }
  */
 class Solution {
-    ListNode temp;
-    boolean flag = true;
+    // ListNode temp;
+    // boolean flag = true;
     public boolean isPalindrome(ListNode head) {
-        temp = head;
-        check(head);
-        return flag;
+        if(head == null || head.next ==null)
+            return true;
+        // temp = head;
+        // check(head);
+        // return flag;
+        ListNode second = head, first = head;
+        while(first != null && first.next != null)
+        {
+            second = second.next;
+            first = first.next.next;
+        }
+        second = reverse(second);
+        return check(head, second);  
     }
     
-    public void check(ListNode head)
-    {
-        if(head == null)
-            return;
-        check(head.next);
-        System.out.println(head.val + " "+ temp.val);
-        if(head.val != temp.val)
-        {
-            flag = false;
+    public boolean check(ListNode first, ListNode second){
+        
+        while(second != null){
+            if(second.val != first.val)
+                return false;
+            second = second.next;
+            first = first.next;
         }
-        temp = temp.next;
+        return true;
     }
+    
+    public ListNode reverse(ListNode head){
+        ListNode prev = null, next;
+        while(head != null){
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+    
+    // public void check(ListNode head)
+    // {
+    //     if(head == null)
+    //         return;
+    //     check(head.next);
+    //     System.out.println(head.val + " "+ temp.val);
+    //     if(head.val != temp.val)
+    //     {
+    //         flag = false;
+    //     }
+    //     temp = temp.next;
+    // }
 }
