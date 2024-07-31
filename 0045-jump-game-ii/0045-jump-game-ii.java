da723787e427e8c0fc2ac3f8798953b1e0fb7e1a
Mohit Agarwal <64399196+mohitagarwal99@users.noreply.github.com>
2024-07-31 23:05:47 +0530

    Time: 615 ms (5.02%), Space: 45.3 MB (31.00%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int jump(int[] nums) {
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        helper(nums, nums.length, 0, dp, 0);
+        return dp[nums.length - 1];
+    }
+    
+    public void helper(int[] nums, int n, int ind, int dp[], int jump){
+        
+        if(ind >= n-1){
+            dp[n-1] = Math.min(dp[n-1], jump);
+            return;
+        }
+        
+//         if(nums[ind] + ind >= n-1){
+//             dp[n-1] = Math.min(dp[n-1], jump+1);
+//             return;
+//         }
+        
+        if(nums[ind] == 0){
+            return;
+        }
+        if(jump >= dp[ind]){
+            return;
+        }
+        dp[ind] = jump;
+        for(int i = nums[ind];  i >= 1; i--){
+            helper(nums, n, ind+i, dp, jump+1);
+            // if(dp[n-1] != Integer.MAX_VALUE)
+            //     return;
+            }
+        
+        return;
+    }
+}
+
